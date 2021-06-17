6a3c0bdf710dbbb3da6773147519dd3c517b4d81
Alivia-m <aliviagayle@icloud.com>
2021-06-17 08:27:41 -0500

    completed

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SyntaxSugar.Exc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 4;
+       
+            var response = (answer < 9) ?  $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+
+            Console.WriteLine(response);
+
+        }
+    }
+}
